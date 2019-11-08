b5282d634af68fc1b73966ad845f603f3d1e6d32
kadir yorulmaz <kadiryorulmaz1@hotmail.com>
2019-11-08 17:17:00 +0100

    helll

--- BLE_TEST_BLENO/Javascript kode.js
+++ BLE_TEST_BLENO/Javascript kode.js
@@ -1,27 +1,28 @@
-  /*GROUP1 = [
-    {sensorName: "Light", uuid: "8998934"},
-    {sensorName: "Radiator", uuid: "8998934"}
-  ]*/
+console.log("Hello");
 
 var devices = [
-  {DeviceId: 1, DeviceName: "Light", WantToSubscribe: false,  groupId: 1},
+  {DeviceId: 1, DeviceName: "Light", WantToSubscribe: true,  groupId: null},
   {DeviceId: 2, DeviceName: "Radiator", WantToSubscribe: true,  groupId: null}
 ];
 
 var DevicesWhoWantToSubscribe = [];
 
 
-
-
-var GROUP;
+var allGroups = [];
 var IsGroupVisible = false;
 
 function AddNewGroup(){
-GROUP = [];
+  let newGroup = {
+    groupId: 0,
+    groupSubscribers: []
+  }
+  allGroups.push(newGroup);
+  console.log(allGroups);
 }
+AddNewGroup();
 
 function DeleteGroup(){
-GROUP = null;
+allGroups = null;
 }
 
 function OpenGroup(){
@@ -33,32 +34,72 @@ function CloseGroup(){
 }
 
 function DeviceWhoWantToSubScribe (){
-  for(let i = 0; i < devices.length; i++ ){
-      if(devices[i].WantToSubscribe && devices[i].groupId === null ){
-    //  console.log(i);
-    //  console.log(devices[i].WantToSubscribe);
-      DevicesWhoWantToSubscribe.push(devices[i]);
+  if(IsGroupVisible === true){
+    for(let i = 0; i < devices.length; i++ ){
+          if(devices[i].WantToSubscribe && devices[i].groupId === null ){
+          console.log(i);
+          console.log(devices[i].WantToSubscribe);
+          DevicesWhoWantToSubscribe.push(devices[i]);
+          }
       }
+      console.log(DevicesWhoWantToSubscribe);
+    return DevicesWhoWantToSubscribe;
+  }else{
+    return "HEY, the group is not open";
   }
-  //console.log(DevicesWhoWantToSubscribe);
-  return DevicesWhoWantToSubscribe;
+  
 }
-
 DeviceWhoWantToSubScribe();
 
 function GetSubscribers (groupIdValue){
-  let subscribers = [];
-  for(let i = 0; i < devices.length; i++ ){
-      if(devices[i].groupId != groupIdValue){
-        console.log(i);
-        console.log(devices[i].WantToSubscribe);
-        subscribers.push(devices[i]);
+  
+  for(let i = 0; i < allGroups.length; i++ ){
+      if(allGroups[i].groupId === groupIdValue){
+        return allGroups[i].groupSubscribers;
       }
   }
-  //console.log(DevicesWhoWantToSubscribe);
-  return subscribers;
 }
-GetSubscribers(1);
+console.log(GetSubscribers(0));
+
+function addSubscribers(deviceId, groupId){
+  let devicesToAdd;
+  for(let j = 0; j < devices.length; j++){
+        if(devices[j].DeviceId === deviceId){
+          console.log(devices[j].DeviceId);
+          console.log(devices[j]);
+          devices[j].groupId = groupId;
+          devicesToAdd = devices[j]; 
+        }
+      }
+  
+  for(let i = 0; i < allGroups.length; i++){
+    if(allGroups[i].groupId === groupId){
+      allGroups[i].groupSubscribers.push(devicesToAdd);
+    }
+  }
+ // GROUP.push(GetSubscribers(1));
+ // console.log(GROUP);
+}
+addSubscribers(1, 0);
+console.log(GetSubscribers(0));
+console.log("SUBSCRIBER:", GetSubscribers(0));
+
+function deleteSubscriber(deviceId, groupId){
+  let devicesToDelete;
+  for(let i = 0; i < allGroups.length; i++){
+    if(allGroups[i].groupId === groupId){
+          $.each(allGroups[i].groupSubscribers, function(j){
+            if(allGroups[i].groupSubscribers[j].DeviceId === deviceId) {
+                allGroups[i].groupSubscribers.splice(j,1);
+                return false;
+            }
+          });
+    }
+  }
+}
+deleteSubscriber(1,0);
+console.log("SUBSCRIBER:", GetSubscribers(0));
+
 
 /*function RandomGroupGenerator (){
   var groupName
