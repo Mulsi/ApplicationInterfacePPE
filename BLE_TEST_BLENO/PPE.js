8373c45cc95705488f649ac83e5fba5962dfd9fc
kadir yorulmaz <kadiryorulmaz1@hotmail.com>
2019-11-08 14:24:53 +0100

    Js file

---
+++ BLE_TEST_BLENO/PPE.js
@@ -0,0 +1,26 @@
+https://raspberrypi.stackexchange.com/questions/11684/how-can-i-connect-my-pi-directly-to-my-pc-and-share-the-internet-connection
+
+pierre.mera@oassiopi.com
+
+patent
+->
+
+New invension 
+
+
+Discovery of electricity is not an invention 
+
+what is your invension
+
+problem for the project was this
+
+solution si s this
+
+our stategy is this 
+
+-> strategy is to keep it secret 
+
+
+https://worldwide.espacenet.com/searchResults?ST=singleline&locale=en_EP&submitted=true&DB=&query=human+machine+interface
+
+https://www.kubii.fr/consoles-retro-gaming/2719-gpi-case-retroflag-3272496299276.html
