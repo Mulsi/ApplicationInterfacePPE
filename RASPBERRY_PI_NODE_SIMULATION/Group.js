4106e194416e08564cea186253ef57364fc40790
Detzy <eirik.olberg@gmail.com>
2019-12-09 15:04:53 +0100

    Made more comments on excisting code

--- RASPBERRY_PI_NODE_SIMULATION/Group.js
+++ RASPBERRY_PI_NODE_SIMULATION/Group.js
@@ -22,9 +22,6 @@ let devices = [
   {deviceId: 2, deviceName: "Radiator", wantToSubscribe: true,  groupId: null}
 ];
 
-let devicesWhoWantToSubscribe = [];
-
-
 let allGroups = [];
 let isGroupVisible = false;
 
@@ -79,17 +76,21 @@ function closeGroup(){
     isGroupVisible = false;
 }
 
-function deviceWhoWantToSubscribe (){
+function devicesWhoWantToSubscribe (){
+    /*
+    This method checks for devices marked with "want to subscribe" that is not in an excistng group,
+    and returns them in a list.
+    Therefore, any device can currently only belong to one group.
+    */
+    let devicesWhoWantToSubscribe = [];
     if(isGroupVisible === true){
-        for(let i = 0; i < devices.length; i++ ){
+        for (let i = 0; i < devices.length; i++ ){
             if(devices[i].wantToSubscribe && devices[i].groupId === null ){
-                console.log(i);
-                console.log(devices[i].wantToSubscribe);
-                devicesWhowantToSubscribe.push(devices[i]);
+                devicesWhoWantToSubscribe.push(devices[i]);
             }
         }
-        console.log(devicesWhowantToSubscribe);
-        return devicesWhowantToSubscribe;
+        console.log(devicesWhoWantToSubscribe);
+        return devicesWhoWantToSubscribe;
     }else{
         return "HEY, the group is not open";
     }
@@ -98,37 +99,44 @@ function deviceWhoWantToSubscribe (){
 //  deviceWhoWantToSubscribe();
 
 function getSubscribers (groupIdValue){
-
+    /*
+    Returns subscribers in the specified group
+    */
   for(let i = 0; i < allGroups.length; i++ ){
       if(allGroups[i].groupId === groupIdValue){
         return allGroups[i].groupSubscribers;
       }
   }
 }
+
 // console.log(getSubscribers(0));
 
 function addSubscribers(deviceId, groupId){
-  let devicesToAdd;
-  for(let j = 0; j < devices.length; j++){
+    /*
+    Add subscriber with given id to group with given id
+    */
+    let devicesToAdd;
+    for(let j = 0; j < devices.length; j++){
         if(devices[j].deviceId === deviceId){
-          console.log(devices[j].deviceId);
-          console.log(devices[j]);
-          devices[j].groupId = groupId;
-          devicesToAdd = devices[j];
+            console.log(devices[j].deviceId);
+            console.log(devices[j]);
+            devices[j].groupId = groupId;
+            devicesToAdd = devices[j];
         }
-      }
-
-  for(let i = 0; i < allGroups.length; i++){
-    if(allGroups[i].groupId === groupId){
-      allGroups[i].groupSubscribers.push(devicesToAdd);
     }
-  }
- // GROUP.push(getSubscribers(1));
- // console.log(GROUP);
+
+    for(let i = 0; i < allGroups.length; i++){
+        if(allGroups[i].groupId === groupId){
+            allGroups[i].groupSubscribers.push(devicesToAdd);
+        }
+    }
+    // GROUP.push(getSubscribers(1));
+    // console.log(GROUP);
 }
-addSubscribers(1, 0);
-console.log(getSubscribers(0));
-console.log("SUBSCRIBER:", getSubscribers(0));
+
+// addSubscribers(1, 0);
+// console.log(getSubscribers(0));
+// console.log("SUBSCRIBER:", getSubscribers(0));
 
 function deleteSubscriber(deviceId, groupId){
   let devicesToDelete;
